e5b645792bad9e9c84c945971436ee23ae482cb2
gaylnabil <gaylnabil@gmail.com>
2024-11-21 02:15:19 +0100

    edit index and grid html

--- js/load.js
+++ js/load.js
@@ -3,5 +3,10 @@ $(function () {
     $.each(includes, function () {
       var file = 'views/' + $(this).data('include') + '.html'
       $(this).load(file)
-    })
+    });
+    // setTimeout(() =>
+    //   {
+    //     document.getElementById("loader").style.display = "none";
+    //     document.getElementById("container").style.display = "block";
+    //   }, 1000);
 })
