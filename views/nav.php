485ec8f5ae5f85eeaa34df5d640f3127a6fcbe3c
gaylnabil <gaylnabil@gmail.com>
2024-11-23 17:09:01 +0100

    from version html to version PHP

---
+++ views/nav.php
@@ -0,0 +1,7 @@
+<nav>
+  <ul class="tc _o sf yo cg ep">
+    <li><a href="index.php" class="xl" :class="{ 'mk': page === 'home' }">Accueil</a></li>
+    <li><a href="blog-grid.php" class="xl" :class="{ 'mk': page === 'blog-grid' }">Services</a></li>
+    <li><a href="faq.php" class="xl">FAQ</a></li>
+  </ul>
+</nav>
